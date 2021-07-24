4e4396a8b95ae265e9c522a1aa7d52b0ad8c8f19
m13100649540@163.com <dlmmczj123>
2021-07-24 21:20:42 +0800

    #comment 格式化文档

--- com.java.main/ThreadTest/ThreadTest.java
+++ com.java.main/ThreadTest/ThreadTest.java
@@ -7,16 +7,16 @@ import java.util.concurrent.TimeUnit;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        ExecutorService executorService = new ThreadPoolExecutor(1,2,100, TimeUnit.MILLISECONDS,  new ArrayBlockingQueue<>(1));
-        for(int j = 0;j<3;j++){
-            executorService.execute(()->{
-                for (int i =0; i<50;i++){
+        ExecutorService executorService = new ThreadPoolExecutor(1, 2, 100, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(1));
+        for (int j = 0; j < 3; j++) {
+            executorService.execute(() -> {
+                for (int i = 0; i < 50; i++) {
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.println("当前线程名称："+Thread.currentThread().getName()+" 数值："+i);
+                    System.out.println("当前线程名称：" + Thread.currentThread().getName() + " 数值：" + i);
                 }
             });
         }
